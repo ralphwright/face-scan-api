a8d21e51c5ce0919d10132c354792412847e69a3
ralphwright <ralphwrightengineer@gmail.com>
2020-02-24 22:37:41 -0600

    adding db connection

--- server.js
+++ server.js
@@ -12,10 +12,8 @@ const image = require("./controllers/image");
 const db = knex({
   client: "pg",
   connection: {
-    host: "postgresql-concave-99451",
-    user: "postgres",
-    password: "admin",
-    database: "face_scan"
+    host: process.env.DATABASE_URL,
+    ssl: true
   }
 });
 
