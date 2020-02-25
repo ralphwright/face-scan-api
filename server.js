0b7936ef30e6f0cc3ad2905e3fa239c9f4563289
ralphwright <ralphwrightengineer@gmail.com>
2020-02-24 22:28:59 -0600

    updating db

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const image = require("./controllers/image");
 const db = knex({
   client: "pg",
   connection: {
-    host: "127.0.0.1",
+    host: "postgresql-concave-99451",
     user: "postgres",
     password: "admin",
     database: "face_scan"
